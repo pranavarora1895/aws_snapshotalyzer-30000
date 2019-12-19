851052fd3b77a8fad3495446ae94300bf0513826
Pranav Arora <aurorapranav187@gmail.com>
2019-12-19 13:58:00 +0530

    Listing of various other fields

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,11 +1,24 @@
 import boto3
+import click
+
+
+
+session = boto3.Session(
+aws_access_key_id = '*youraccesskey*',
+aws_secret_access_key = '*yoursecretaccesskey*')
+ec2 = session.resource('ec2','us-east-1')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(','.join((i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
 
 if __name__=='__main__':
-    session = boto3.Session(
-    aws_access_key_id = 'AKIAVCU5LPAZKRLU57UC',
-    aws_secret_access_key = '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')
 
-
-    ec2 = session.resource('ec2','us-east-1')
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
